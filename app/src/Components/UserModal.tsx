ee2ec142b3cfed712e1622ad97ef9674b91ffb84
gech <getasewbekahegn@gmail.com>
2023-10-08 20:57:34 +0300

    util.js

--- app/src/Components/UserModal.tsx
+++ app/src/Components/UserModal.tsx
@@ -1,42 +1,61 @@
 import React, { useState } from 'react';
 import { Button, Modal, Input } from 'antd';
+import { useQueryClient } from 'react-query'; // Import queryClient from React Query
 
+const UserModal: React.FC = ({ createUserMutation, updateUserMutation }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone_number, setPhoneNumber] = useState('');
 
+  const queryClient = useQueryClient(); // Get queryClient
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
-const UserModal: React.FC = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [phone_number, setPhoneNumber] = useState();
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
 
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
-    const handleOk = () => {
-        setIsModalOpen(false);
-    };
+  const handleCreateUser = () => {
+    createUserMutation.mutate({ name, email, password, phone_number }, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users'); 
+        handleOk();
+      },
+    });
+  };
 
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
+  const handleUpdateUser = () => {
+    updateUserMutation.mutate({ id: userId, name, email, password, phone_number }, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users'); 
+        handleOk();
+      },
+    });
+  };
 
-    console.log(name,email, password,phone_number);
-    return (
-        <>
-            <Button type="primary" style={{ float: "right" }} onClick={showModal}>
-                Add User
-            </Button>
-            <Modal title="New User Add Form" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+  // ... Rest of the code ...
+
+  return (
+    <>
+      <Button type="primary" style={{ float: 'right' }} onClick={showModal}>
+        Add User
+      </Button>
+      <Modal title="New User Add Form" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
                 <Input className='pt-2' placeholder='Enter User Name' onChange={(e: any) => setName(e.target.value)} ></Input>
                 <Input className='pt-2' placeholder='Enter Email' onChange={(e: any) => setEmail(e.target.value)} ></Input>
                 <Input className='pt-2' placeholder='Enter Password' onChange={(e: any) => setPassword(e.target.value)} ></Input>
                 <Input className='pt-2' placeholder='Enter Phone Number' onChange={(e: any) => setPhoneNumber(e.target.value)}></Input>
             </Modal>
-        </>
-    );
+    </>
+  );
 };
 
 export default UserModal;
--- app/src/Pages/Users.tsx
+++ app/src/Pages/Users.tsx
@@ -1,133 +1,138 @@
 import React, { useState } from 'react';
-import type { ColumnsType } from 'antd/es/table';
-import type { TableRowSelection } from 'antd/es/table/interface';
-import UserModal from '../Components/UserModal';
-import './Users.css'; 
-import Table from 'antd/es/table';
+import { useQuery, useMutation } from 'react-query';
 import {
-    EditOutlined,
-    DeleteOutlined,
-  } from '@ant-design/icons';
-import { Button } from 'antd';
+  EditOutlined,
+  DeleteOutlined,
+} from '@ant-design/icons';
+import { Button, Table } from 'antd';
+import UserModal from '../Components/UserModal';
+import type { ColumnsType } from 'antd/es/table';
+import axiosInstance from '../util/util';
 
 
 
 interface DataType {
-    key: React.Key;
-    name: string;
-    email: string;
-    password: string;
-    phone_number: string;
+  key: React.Key;
+  name: string;
+  email: string;
+  password: string;
+  phone_number: string;
 }
 
 const columns: ColumnsType<DataType> = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-    },
-    {
-        title: 'Password',
-        dataIndex: 'password',
-    },
-    {
-        title: 'Phone Number',
-        dataIndex: 'phone_number',
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (text, record) => (
-          <span>
-            {/* Edit Icon Button */}
-            <Button
-              type="primary"
-              icon={<EditOutlined />}
-              onClick={() => handleEdit(record.key)}
-              style={{ marginRight: 8 }}
-            />
-            {/* Delete Icon Button */}
-            <Button
-             className="red-button"
-              icon={<DeleteOutlined />}
-              onClick={() => handleDelete(record.key)}
-            />
-          </span>
-        ),
-      },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+  {
+    title: 'Password',
+    dataIndex: 'password',
+  },
+  {
+    title: 'Phone Number',
+    dataIndex: 'phone_number',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        {/* Edit Icon Button */}
+        <Button
+          type="primary"
+          icon={<EditOutlined />}
+          onClick={() => handleEdit(record.key)}
+          style={{ marginRight: 8 }}
+        />
+        {/* Delete Icon Button */}
+        <Button
+          className="red-button"
+          icon={<DeleteOutlined />}
+          onClick={() => handleDelete(record.key)}
+        />
+      </span>
+    ),
+  },
 ];
 
-const data: DataType[] = [];
-for (let i = 0; i < 46; i++) {
-    data.push({
-        key: i,
-        name: "Edward King ${i}",
-        email: "getasewbekahegn@gmail.com",
-        password: "12345678",
-        phone_number: "092345678",
-    });
-}
 
 const Users: React.FC = () => {
-    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const { data, isLoading, isError, error } = useQuery([users], async () => {
+    const response = await axiosInstance.get('/api/v1/user');
+    return response.data.result;
+  });
 
-    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-        console.log('selectedRowKeys changed: ', newSelectedRowKeys);
-        setSelectedRowKeys(newSelectedRowKeys);
-    };
+ 
 
-    const rowSelection: TableRowSelection<DataType> = {
-        selectedRowKeys,
-        onChange: onSelectChange,
-        selections: [
-            Table.SELECTION_ALL,
-            Table.SELECTION_INVERT,
-            Table.SELECTION_NONE,
-            {
-                key: 'odd',
-                text: 'Select Odd Row',
-                onSelect: (changeableRowKeys) => {
-                    let newSelectedRowKeys = [];
-                    newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
-                        if (index % 2 !== 0) {
-                            return false;
-                        }
-                        return true;
-                    });
-                    setSelectedRowKeys(newSelectedRowKeys);
-                },
-            },
-            {
-                key: 'even',
-                text: 'Select Even Row',
-                onSelect: (changeableRowKeys) => {
-                    let newSelectedRowKeys = [];
-                    newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
-                        if (index % 2 !== 0) {
-                            return true;
-                        }
-                        return false;
-                    });
-                    setSelectedRowKeys(newSelectedRowKeys);
-                },
-            },
-        ],
-    };
-
-    return (
-        <div className="users-container"> {/* Apply the CSS class */}
-            <div>
-                <UserModal></UserModal>
-            </div>
-            <Table columns={columns} dataSource={data} />
+  // Use React Query mutations for create, update, and delete
+  const createUserMutation = useMutation((user) => axiosInstance.post('/api/v1/user', user));
+  const updateUserMutation = useMutation((data) => {
+    const { id, ...userData } = data;
+    return axiosInstance.put(`/api/v1/user/${id}`, userData);
+  });
+  const deleteUserMutation = useMutation((id) => axiosInstance.delete(`/api/v1/user/${id}`));
 
 
-            
-        </div>
-    );
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection: TableRowSelection<DataType> = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: 'odd',
+        text: 'Select Odd Row',
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return false;
+            }
+            return true;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+      {
+        key: 'even',
+        text: 'Select Even Row',
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return true;
+            }
+            return false;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="users-container">
+      <div>
+        <UserModal
+          createUserMutation={createUserMutation}
+          updateUserMutation={updateUserMutation}
+        />
+      </div>
+      <Table columns={columns} dataSource={users} />
+    </div>
+  );
 };
 
 export default Users;
